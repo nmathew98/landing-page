72f31e92c70452674c43244ca8fe628659a69bfa
Naveen Mathew <naveenmat98+github@gmail.com>
2020-11-08 18:04:42 +1300

    Added Prismic

---
+++ src/app/services/prismic.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import * as Prismic from 'prismic-javascript';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class PrismicService {
+	constructor() {}
+
+	async query(repository_name: string, predicates: any[], orderings: string): Promise<any> {
+		let result = [];
+
+		await Prismic.getApi(`https://${repository_name}.prismic.io/api/v2`)
+			.then(function(api) {
+				return api.query(predicates, { orderings: `${orderings}` });
+			})
+			.then(
+				function(response) {
+					result = response.results;
+				},
+				function(err) {
+					console.log('Something went wrong: ', err);
+				}
+			);
+
+		return result;
+	}
+
+	async queryByUID(repository_name: string, type: string, uid: string): Promise<any> {
+		let result = {};
+		await Prismic.getApi(`https://${repository_name}.prismic.io/api/v2`)
+			.then(function(api) {
+				return api.getByUID(type, uid);
+			})
+			.then(
+				function(response) {
+					result = response;
+				},
+				function(err) {
+					console.log('Something went wrong: ', err);
+				}
+			);
+
+		return result;
+	}
+}
